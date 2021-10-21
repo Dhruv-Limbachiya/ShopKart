15a9a97c551ff201dc14f823f27faaafa60c8ffc
Dhruv-Limbachiya <dhruv.mail4work@gmail.com>
2021-10-21 17:51:16 +0530

    set up bottom navigation using Nav Component

--- app/src/main/java/com/example/shopkart/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/shopkart/ui/activities/DashboardActivity.kt
@@ -1,10 +1,15 @@
 package com.example.shopkart.ui.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shopkart.R
+import com.example.shopkart.databinding.ActivityDashboardBinding
 import com.example.shopkart.ui.activities.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,17 +17,54 @@ import dagger.hilt.android.AndroidEntryPoint
 class DashboardActivity : BaseActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var mBinding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
 
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_dashboard)
+
+        setUpNavComp()
+
+        navigateToProfileOnInCompleteProfile()
+    }
+
+    /**
+     * Configures the require navigation stuff.
+     */
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setUpNavComp() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment // Nav Host
+
+        navController = navHostFragment.navController // find NavController using NavHostFragment/
+
+
+        // Configure App bar by specifying all top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.dashboardFragment, R.id.productFragment, R.id.orderFragment
+            )
+        )
+
+        supportActionBar?.setBackgroundDrawable(
+            getDrawable(R.drawable.drawable_gradient_splash_background)
+        )
+
+        // set nav controller and app bar configuration instance to the default action bar.
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // set nav controller to bottom navigation view for automatically navigating between fragments.
+        mBinding.bottomNavView.setupWithNavController(navController)
+    }
+
+    /**
+     * Navigates to [ProfileFragment] if the profile is incomplete.
+     */
+    private fun navigateToProfileOnInCompleteProfile() {
         intent?.let {
-            val isProfileCompleted = it.getIntExtra(getString(R.string.prefIsProfileCompleted),-1)
-            if(isProfileCompleted == 0) {
-                val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-
-                navController = navHostFragment.navController
+            val isProfileCompleted = it.getIntExtra(getString(R.string.prefIsProfileCompleted), -1)
+            if (isProfileCompleted == 0) {
                 navController
                     .navigate(R.id.action_global_profileFragment)
             }
