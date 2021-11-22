2801d10d693a293f6c3b0bf946c89c6f4659154b
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-11-22 10:15:08 +0530

    CheckoutFragment's UI

---
+++ app/src/main/java/com/example/shopkart/ui/fragments/checkout/CheckoutFragment.kt
@@ -0,0 +1,9 @@
+package com.example.shopkart.ui.fragments.checkout
+
+import com.example.shopkart.ui.fragments.base.BaseFragment
+
+/**
+ * Created By Dhruv Limbachiya on 22-11-2021 09:47 AM.
+ */
+class CheckoutFragment : BaseFragment() {
+}
